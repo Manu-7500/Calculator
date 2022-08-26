21821ce8a67c160800ce763461b46031742a6cd6
MANU BHARADWAJ <pandatmanu318@gmail.com>
2022-08-26 12:34:45 +0300

    added js in calculator

---
+++ app.js
@@ -0,0 +1,48 @@
+let screen = document.getElementById( 'screen' );
+buttons = document.querySelectorAll( 'button' );
+let screenValue = '';
+for ( item of buttons ) {
+
+    item.addEventListener( 'click', ( e ) => {
+        buttonText = e.target.innerText;
+        console.log( buttonText );
+
+        if ( buttonText == 'x' ) {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if ( buttonText == '=' ) {
+            screen.value = eval( screenValue );
+
+        }
+        else if ( buttonText == 'C' ) {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if ( buttonText == 'D' ) {
+            screenValue = screenValue.slice( 0, -1 );
+            screen.value = screenValue;
+        }
+        else if ( buttonText == '%' ) {
+            buttonText = '/';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if ( buttonText == '[' ) {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if ( buttonText == ']' ) {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    } )
+} 
